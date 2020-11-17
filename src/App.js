4f2fa76894af87e6572dd000c1e7016fd87c0c6d
Mau Abata <mauabata@gmail.com>
2020-11-17 03:19:59 -0600

    Fetch version data from Refsheet backend.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import React from 'react'
-import logo from './logo.svg'
+import React, {useEffect, useState} from 'react'
+import logo from './assets/RS_Logo_SVG.svg'
 import './App.css'
+import client from "./services/RefsheetApiService"
+import {ApolloProvider} from "@apollo/client"
+import {Query} from "@apollo/client/react/components"
+
+import { loader } from 'graphql.macro'
+const getVersion = loader('./getVersion.graphql')
+
+const TimedVersion = ({loading, data, error}) => {
+  const [start, setStart] = useState(Date.now())
+  const [time, setTime] = useState(0)
+
+  useEffect(() => {
+    if (loading) {
+      setStart(Date.now())
+    }
+    setTime(Date.now() - start)
+  }, [loading])
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  if (data.getVersion) {
+    return (
+      <div>Connected to version {data.getVersion.version} in {time}ms.</div>
+    )
+  }
+
+  return (
+    <div>Something went sadly incorrect.</div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ApolloProvider client={client}>
+      <div className="App">
+        <header className="App-header">
+          <img width={150} src={logo} className="App-logo" alt="logo" />
+          <Query query={getVersion}>
+            {({loading, data, error}) => (
+              <TimedVersion loading={loading} data={data} error={error} />
+            )}
+          </Query>
+        </header>
+      </div>
+    </ApolloProvider>
   )
 }
 
---
+++ src/services/RefsheetApiService.js
@@ -0,0 +1,16 @@
+import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
+
+const HOST = 'http://localhost:5000'
+
+const httpLink = new HttpLink({
+  uri: `${HOST}/graphql`,
+  credentials: 'include',
+})
+
+export const client = new ApolloClient({
+  cache: new InMemoryCache(),
+  link: httpLink
+})
+
+export { HOST as host }
+export default client
