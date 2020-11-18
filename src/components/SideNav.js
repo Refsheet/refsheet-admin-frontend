06ecff50f9602cc3dd7518bb61c3b74d174ae8b2
Mau Abata <mauabata@gmail.com>
2020-11-18 17:19:17 -0600

    Sidenav populated kinda like the old admin panel.

--- src/components/SideNav.js
+++ src/components/SideNav.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Icon, SideNav } from 'react-materialize'
+import { SideNavItem, Icon, SideNav } from 'react-materialize'
 import { NavLink } from 'react-router-dom'
 
 const Sidebar = () => {
@@ -11,29 +11,58 @@ const Sidebar = () => {
       }}
       fixed
     >
-      <li>
+      <li style={{ paddingTop: 8 }}>
         <NavLink to={'/'} exact activeClassName={'primary-dark white-text'}>
           <Icon className={'prefix'}>dashboard</Icon> Dashboard
         </NavLink>
       </li>
       <li>
-        <NavLink to={'/settings'} activeClassName={'primary-dark white-text'}>
-          <Icon className={'prefix'}>settings</Icon> Settings
+        <NavLink to={'/moderation'} activeClassName={'primary-dark white-text'}>
+          <Icon className={'prefix'}>report</Icon> Moderation
+        </NavLink>
+      </li>
+
+      <SideNavItem divider />
+
+      <li style={{ paddingTop: 8 }}>
+        <NavLink to={'/users'} activeClassName={'primary-dark white-text'}>
+          <Icon className={'prefix'}>person</Icon> Users
         </NavLink>
       </li>
       <li>
-        <NavLink to={'/console'} activeClassName={'primary-dark white-text'}>
-          <Icon className={'prefix'}>code</Icon> Serial Console
+        <NavLink to={'/characters'} activeClassName={'primary-dark white-text'}>
+          <Icon className={'prefix'}>face</Icon> Characters
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to={'/artists'} activeClassName={'primary-dark white-text'}>
+          <Icon className={'prefix'}>people</Icon> Artists
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to={'/images'} activeClassName={'primary-dark white-text'}>
+          <Icon className={'prefix'}>collections</Icon> Images
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to={'/forums'} activeClassName={'primary-dark white-text'}>
+          <Icon className={'prefix'}>messages</Icon> Forums
         </NavLink>
       </li>
 
-      {/*<SideNavItem href="#!second">*/}
-      {/*Second Link*/}
-      {/*</SideNavItem>*/}
+      <SideNavItem divider />
 
-      {/*<SideNavItem subheader>*/}
-      {/*Subheader*/}
-      {/*</SideNavItem>*/}
+      <li style={{ paddingTop: 8 }}>
+        <NavLink to={'/supporters'} activeClassName={'primary-dark white-text'}>
+          <Icon className={'prefix'}>monetization_on</Icon> Supporters
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to={'/ads'} activeClassName={'primary-dark white-text'}>
+          <Icon className={'prefix'}>open_in_new</Icon> Advertising
+        </NavLink>
+      </li>
+
     </SideNav>
   )
 }
