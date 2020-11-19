91534a3d3126bbcf3c910ea87b8cd35e73ab0665
Mau Abata <mauabata@gmail.com>
2020-11-19 00:54:35 -0600

    GodDAMN that's a sexy search bar.

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
 import { Button, Icon } from 'react-materialize'
 
-const SearchBar = ({ onSearch = () => {} }) => {
-  const [query, setQuery] = useState('')
+const SearchBar = ({ onSearch = () => {}, query: defaultQuery = '' }) => {
+  const [query, setQuery] = useState(defaultQuery)
 
   const handleSubmit = (e) => {
     e.preventDefault()
@@ -14,18 +14,41 @@ const SearchBar = ({ onSearch = () => {} }) => {
     setQuery(e.target.value)
   }
 
+  const handleClear = (e) => {
+    e.preventDefault()
+    setQuery('')
+    onSearch('')
+  }
+
   return (
-    <form className={'SearchBar'} onSubmit={handleSubmit}>
-      <input
-        type={'search'}
-        placeholder={'What is it you seek, good hunter?'}
-        value={query}
-        onChange={handleChange}
-      />
-      <Button type={'submit'} disabled={!query}>
-        <Icon>search</Icon>
-      </Button>
-    </form>
+    <React.Fragment>
+      <form className={'SearchBar'} onSubmit={handleSubmit}>
+        <input
+          type={'search'}
+          placeholder={'What is it you seek, good hunter?'}
+          value={query}
+          onChange={handleChange}
+        />
+        <Button type={'submit'} disabled={!query}>
+          <Icon>search</Icon>
+        </Button>
+      </form>
+
+      <div className={'grey-text text-darken-2 search-results'}>
+        {defaultQuery ? (
+          <React.Fragment>
+            <span className={'right'}>
+              <a href={'#'} onClick={handleClear}>
+                clear
+              </a>
+            </span>
+            Results for: <strong>{defaultQuery}</strong>
+          </React.Fragment>
+        ) : (
+          ''
+        )}
+      </div>
+    </React.Fragment>
   )
 }
 
--- src/components/pages/Users/index.js
+++ src/components/pages/Users/index.js
@@ -1,5 +1,6 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import {
+  Preloader,
   Row,
   Col,
   Collection,
@@ -9,6 +10,12 @@ import {
 } from 'react-materialize'
 import { SessionContext } from '../../providers/SessionProvider'
 import SearchBar from '../../SearchBar'
+import Scrollbar from 'react-scrollbars-custom'
+import { withContentRect } from 'react-measure'
+import { Query } from '@apollo/client/react/components'
+import { loader } from 'graphql.macro'
+
+const getUsers = loader('../../../graphql/getUsers.graphql')
 
 const UserListItem = ({ user }) => {
   if (!user) return null
@@ -23,60 +30,120 @@ const UserListItem = ({ user }) => {
       </a>
       <p>@{user.username}</p>
       <a className="secondary-content" href="javascript:void(0)">
-        <Icon>flag</Icon>
+        <Icon className={'grey-text'}>star_outline</Icon>
       </a>
     </CollectionItem>
   )
 }
 
-const UserList = () => {
-  const session = useContext(SessionContext)
-  const users = [
-    session.currentUser,
-    session.currentUser,
-    session.currentUser,
-    session.currentUser,
-    session.currentUser,
-    session.currentUser,
-    session.currentUser,
-    session.currentUser,
-    session.currentUser,
-    session.currentUser,
-  ]
+const UserList = withContentRect('bounds')(
+  ({
+    title,
+    search,
+    scope,
+    query,
+    measureRef,
+    contentRect,
+    totalPages = 1,
+  }) => {
+    const session = useContext(SessionContext)
+    const [page, setPage] = useState(1)
 
-  return (
-    <Collection
-      className={'z-depth-1 no-border scrolling'}
-      header={'Problem Children'}
-    >
-      {users.map((user, i) => (
-        <UserListItem key={i} user={user} />
-      ))}
-    </Collection>
-  )
-}
+    const pageGo = (diff) => (e) => {
+      e.preventDefault()
+      setPage(Math.max(1, Math.min(page + diff, totalPages)))
+    }
+
+    return (
+      <div className={'CollectionContainer'} ref={measureRef}>
+        {title && <div className={'collection-header'}>{title}</div>}
+        <Query query={query} variables={{ page: page, query: search }}>
+          {({ data, loading, error }) =>
+            loading ? (
+              <Preloader size={'big'} />
+            ) : error ? (
+              <div>{JSON.stringify(error)}</div>
+            ) : (
+              <React.Fragment>
+                <Collection className={'no-border'}>
+                  <Scrollbar style={{ width: contentRect.width, height: 400 }}>
+                    {data.getUsers &&
+                      data.getUsers.map((user, i) => (
+                        <UserListItem key={i} user={user} />
+                      ))}
+                  </Scrollbar>
+                </Collection>
+                <div className={'collection-footer'}>
+                  <Button
+                    className={'left'}
+                    onClick={pageGo(-1)}
+                    disabled={page <= 1}
+                  >
+                    <Icon>keyboard_arrow_left</Icon>
+                  </Button>
+                  <div className={'center'}>
+                    Page {page} of {totalPages}
+                  </div>
+                  <Button
+                    className={'right'}
+                    onClick={pageGo(1)}
+                    disabled={page >= totalPages}
+                  >
+                    <Icon>keyboard_arrow_right</Icon>
+                  </Button>
+                </div>
+              </React.Fragment>
+            )
+          }
+        </Query>
+      </div>
+    )
+  }
+)
 
 const Users = () => {
+  const [query, setQuery] = useState('')
+
   return (
     <div className={'container-large'}>
       <Row>
         <Col s={12} m={8} offset={'m2'}>
-          <SearchBar />
+          <SearchBar onSearch={setQuery} query={query} />
         </Col>
       </Row>
 
       <Row>
         <Col s={12} m={6} l={3}>
-          <UserList />
+          <UserList
+            query={getUsers}
+            title={'Recently Viewed'}
+            search={query}
+            totalPages={4}
+          />
         </Col>
         <Col s={12} m={6} l={3}>
-          <UserList />
+          <UserList
+            query={getUsers}
+            title={'Problem Children'}
+            search={query}
+            totalPages={4}
+          />
         </Col>
         <Col s={12} m={6} l={3}>
-          <UserList />
+          <UserList
+            query={getUsers}
+            title={'Recently Created'}
+            search={query}
+            totalPages={4}
+          />
         </Col>
         <Col s={12} m={6} l={3}>
-          <UserList />
+          <UserList
+            query={getUsers}
+            title={'Recently Reported'}
+            search={query}
+            totalPages={4}
+          />
         </Col>
       </Row>
     </div>
