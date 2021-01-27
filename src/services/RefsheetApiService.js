060ca3e9731e1a449fa0302c303b2fcb80f9f226
Mau Abata <mauabata@gmail.com>
2021-01-27 05:17:51 -0600

    Staging switch.

--- src/services/RefsheetApiService.js
+++ src/services/RefsheetApiService.js
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache, HttpLink } from '@apollo/client'
 
 const HOST = window.location.host.match(/refsheet.net$/)
-  ? 'https://staging.refsheet.net'
+  ? (window.location.hash === '#staging' ? 'https://staging.refsheet.net' : 'https://refsheet.net')
   : 'http://localhost:5000'
 
 const httpLink = new HttpLink({
